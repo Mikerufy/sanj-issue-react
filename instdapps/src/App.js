2ff63eb720a71f8f2660d26976aae9fece582922
Isha Nalawade <isha.nalawade@gmail,com>
2021-10-24 13:36:36 +0530

    Building defi color style

--- instdapps/src/App.js
+++ instdapps/src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 import {Grid, Typography} from '@mui/material'
 import Appbar from './components/Appbar'
 import Part1 from './components/Part1/Part1';
+import { makeStyles } from "@mui/styles";
+import { createTheme, ThemeProvider } from '@material-ui/core/styles';
+
 
 function App() {
   return (
@@ -14,7 +17,7 @@ function App() {
         <Grid item xs={12} container>
           <Grid item xs={2}></Grid>
           <Grid item xs={8}>
-            <Part1/>
+          <Part1/>
             
           </Grid>
           <Grid item xs={2}></Grid>
--- instdapps/src/components/Part1/Part1.jsx
+++ instdapps/src/components/Part1/Part1.jsx
@@ -2,7 +2,9 @@ import * as React from "react";
 import { Grid, Typography } from "@mui/material";
 import Part1img from "../../images/Part1.png";
 import { makeStyles } from "@mui/styles";
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 import Button from "@mui/material/Button";
+import "./Part1.css";
 
 const useStyles = makeStyles((theme) => ({
     part1: {
@@ -20,16 +22,17 @@ const Part1 = () => {
       <Grid item xs={10} container spacing ={2}>
           <Grid item xs = {6} container >
               <Grid item xs={12}>
-                  <Typography>Building</Typography>
-                  <Typography>Building</Typography>
-                  <Typography>Building</Typography>
+                <Grid container >
+                <Grid item xs={7}><Typography className="building">Building</Typography></Grid>
+                <Grid item xs={5}><Typography className="defi">DeFi</Typography></Grid></Grid>
+                <Typography className="building">Infrastructure</Typography>
               </Grid>
               <Grid item xs={12}>
                   <Typography>The world's most advanced platform to start leveraging the full potential of Decentralised Finance.</Typography>
               </Grid>
               <Grid item xs={12} container spacing = {2}>
-                <Grid item xs = {6}><Button variant = "outlined">Dashboard</Button></Grid>
-                <Grid item xs = {6}><Button variant = "outlined">Dashboard</Button></Grid>
+                <Grid item xs = {6}><Button style={{textTransform: 'titlecase'}} variant = "outlined">Dashboard</Button></Grid>
+                <Grid item xs = {6}><Button style={{textTransform: 'titlecase'}} variant = "outlined">Dashboard</Button></Grid>
               </Grid>
           </Grid>
           <Grid item xs = {6}>
